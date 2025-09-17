7eeb27174d70d6c5be4acdec15a1f3f4f6e86ecc
X189760AvieRg <06.ducky-manses@icloud.com>
2025-09-17 09:40:43 +0100

    Add new 表单数据验证器 feature

---
+++ form_validator_0917_1640_qvj.py
@@ -0,0 +1,6 @@
+# 代码生成时间: 2025-09-17 16:40:41
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QLabel, QPushButton, QVBoxLayout, QFormLayout, QMessageBox
+from PyQt5.QtCore import Qt
+
+"
