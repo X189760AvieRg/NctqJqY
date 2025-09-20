0c2ecd5e36a26bcb1d982c126962bf30b78f058a
X189760AvieRg <06.ducky-manses@icloud.com>
2025-09-20 15:25:30 +0100

    Develop new 图片尺寸批量调整器 feature

---
+++ image_resizer_0920_2225_fhc.py
@@ -0,0 +1,9 @@
+# 代码生成时间: 2025-09-20 22:25:29
+import sys
+import os
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QFileDialog, QLabel
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+import PIL.Image as Image
+
+"
