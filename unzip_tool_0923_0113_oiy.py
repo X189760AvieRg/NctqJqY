2aa3f38bf91b94908412f11fc5036e3c8b25aaa2
X189760AvieRg <06.ducky-manses@icloud.com>
2025-09-22 18:13:28 +0100

    Implement 压缩文件解压工具

---
+++ unzip_tool_0923_0113_oiy.py
@@ -0,0 +1,7 @@
+# 代码生成时间: 2025-09-23 01:13:27
+import sys
+import zipfile
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QVBoxLayout, QWidget, QPushButton, QProgressBar, QLabel
+from PyQt5.QtCore import pyqtSlot, Qt
+
+"
