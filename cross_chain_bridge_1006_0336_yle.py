e60a070e71c078c376498cbe5e4a05973b5bfaca
X189760AvieRg <06.ducky-manses@icloud.com>
2025-10-05 20:36:23 +0100

    Develop 跨链桥接工具

---
+++ cross_chain_bridge_1006_0336_yle.py
@@ -0,0 +1,14 @@
+# 代码生成时间: 2025-10-06 03:36:22
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel
+from PyQt5.QtCore import pyqtSlot
+
+class CrossChainBridge(QMainWindow):
+    """跨链桥接工具的主窗口"""
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        "
